3bced40b3deb11d1c24c06bd94650fff50e7b73a
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2023-11-16 23:02:51 -0500

    Revise Dockerfile/comments

--- docker/build_container.sh
+++ docker/build_container.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# assumes it's executed directly from this directory and 
+# that a build directory "build_docker" does not exist (see Dockerfile)
 cd ..
 docker build -t raytracer-challenge-container -f docker/Dockerfile .
--- docker/run_container_current_src.sh
+++ docker/run_container_current_src.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# NOTE: expectation is that this script is executed directly from this directory (see the hardcoded paths)
 # the directory "/raytracer-challenge" is already copied to the container, so we'll just mount new one
 # however this will require re-running the generator step; best to make container specific directories
 # if this approach is to be used
-docker run -v ..:/raytracer-challenge.current -it raytracer-challenge-container /bin/bash
+cd ..
+docker run -v `pwd`:/raytracer-challenge.current -it raytracer-challenge-container /bin/bash
