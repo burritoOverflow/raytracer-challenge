26a2f34f3d605952e5ca4dc8c6b76c7d8627692f
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2023-11-17 01:07:13 -0500

    Various changes to Docker build.

--- docker/build_container.sh
+++ docker/build_container.sh
@@ -3,4 +3,13 @@
 # assumes it's executed directly from this directory and 
 # that a build directory "build_docker" does not exist (see Dockerfile)
 cd ..
-docker build -t raytracer-challenge-container -f docker/Dockerfile .
+
+CONTAINER_TOOL=docker
+if  command -v podman &> /dev/null
+then
+    echo "podman found; using podman"
+    CONTAINER_TOOL=podman
+fi
+
+echo "building with ${CONTAINER_TOOL}"
+$CONTAINER_TOOL build -t raytracer-challenge-container -f docker/Dockerfile .
--- docker/run_container_current_src.sh
+++ docker/run_container_current_src.sh
@@ -4,5 +4,14 @@
 # the directory "/raytracer-challenge" is already copied to the container, so we'll just mount new one
 # however this will require re-running the generator step; best to make container specific directories
 # if this approach is to be used
+
+CONTAINER_TOOL=docker
+if  command -v podman &> /dev/null
+then
+    echo "podman found; using podman"
+    CONTAINER_TOOL=podman
+fi
+
 cd ..
-docker run -v `pwd`:/raytracer-challenge.current -it raytracer-challenge-container /bin/bash
+echo "running with ${CONTAINER_TOOL}"
+$CONTAINER_TOOL run -v `pwd`:/raytracer-challenge.current -it raytracer-challenge-container /bin/bash
